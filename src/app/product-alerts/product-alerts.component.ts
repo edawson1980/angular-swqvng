b2722fd635e543db3d4c954c89b4c68cd6a85244
edawson1980 <erinedawson@gmail.com>
2019-07-30 10:58:48 -0600

    update to allow for notify event

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import {Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -9,6 +10,9 @@ import { Input } from '@angular/core';
 export class ProductAlertsComponent implements OnInit {
   // @Input - property's value will be passed in from component's parent (product list)
   @Input() product;
+  // allow for the alert component to emit an event when notify changes (when the notify button is clicked)
+  @Output() notify = new EventEmitter();
+  
   constructor() { }
 
   ngOnInit() {
