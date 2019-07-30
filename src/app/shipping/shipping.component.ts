322ba17e73b759ea3629276dfe853d4e24a31c7d
edawson1980 <erinedawson@gmail.com>
2019-07-30 13:36:40 -0600

    define shippingCosts property

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -7,6 +7,7 @@ import { CartService} from '../cart.service';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts;
 
   constructor() { }
 
