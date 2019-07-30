02d3b5b5998c4b5c596affa31a09a80140f858ad
edawson1980 <erinedawson@gmail.com>
2019-07-30 13:37:07 -0600

    inject cart service

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -9,7 +9,7 @@ import { CartService} from '../cart.service';
 export class ShippingComponent implements OnInit {
   shippingCosts;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit() {
   }
