8cd65e2183537257f67bb1655e7e7fa70bab8b08
edawson1980 <erinedawson@gmail.com>
2019-07-30 13:16:47 -0600

    set cart items using getItems method

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,8 +7,13 @@ import {CartService} from '../cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  //defines the property that stores items in the cart:
+  items;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService) { 
+    //displays the cart items for this instance/iteration of the cart
+    this.items = this.cartService.getItems();
+  }
 
   ngOnInit() {
   }
