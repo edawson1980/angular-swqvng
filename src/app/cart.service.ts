c2af6c1de1be3689042fc7cc454f571a029d3ea2
edawson1980 <erinedawson@gmail.com>
2019-07-30 12:07:49 -0600

    add methods to add items, list items, and clear cart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,7 +2,24 @@ import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class CartService {
+  //items holds the list of what's currently in the cart
   items = [];
+
+  //to add items to cart:
+  addToCart(product){
+    this.items.push(product);
+  }
+
+  //to display what is in cart:
+  getItems(){
+    return this.items;
+  }
+
+  //to clear the cart (reset cart to an empty array):
+  clearCart(){
+    this.items = [];
+    return this.items;
+  }
 
   constructor() { }
 
